1bbdfb2756092e2326babd0bafa0261e54847e24
Srijith Legend <179341048+SrijithLegend@users.noreply.github.com>
2026-07-01 12:35:28 +0530

    Updated vite.config.ts

--- src/entries/popup.tsx
+++ src/entries/popup.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Popup } from '@/components/Popup';
-import '@/styles.css';
+import '@/styles/index.css';
 
 const root = document.getElementById('root');
 if (root) {
